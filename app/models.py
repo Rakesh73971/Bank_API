287b24b8a58fb700c545c67b8879811e5592e3ba
Rakesh N <nraki522@gmail.com>
2026-03-11 22:55:54 +0530

    updated the models

--- app/models.py
+++ app/models.py
@@ -5,14 +5,14 @@ from .database import Base
 class Bank(Base):
     __tablename__ = "banks"
 
-    id = Column(Integer,primary_key=True,nullable=False)
+    id = Column(Integer,primary_key=True,index=True)
     name = Column(String,nullable=False)
     branches = relationship("Branch",back_populates="bank")
 
 class Branch(Base):
     __tablename__ = "branches"
 
-    id = Column(Integer,primary_key=True,nullable=False)
+    id = Column(Integer,primary_key=True,index=True)
     bank_id = Column(Integer,ForeignKey("banks.id"))
     branch_name = Column(String,nullable=False)
     city = Column(String,nullable=False)
