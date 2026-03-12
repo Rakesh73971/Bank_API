e8c5e553a11cba36fc9f8ef9ec7dff2a790db8cd
Rakesh N <nraki522@gmail.com>
2026-03-12 20:48:22 +0530

    tested bank and branch apis

--- tests/test_banks.py
+++ tests/test_banks.py
@@ -32,6 +32,27 @@ def test_get_bank_by_id(client):
     assert response.status_code == 200
     assert response.json()["name"] == "HDFC"
 
+def test_get_bank_branches(client):
+
+    bank = client.post("/banks/", json={"name": "Axis"})
+    bank_id = bank.json()["id"]
+
+    client.post("/branches/", json={
+        "branch_name": "Axis Branch 1",
+        "city":"Hyderabad",
+        "ifsc": "AXIS0001",
+        "bank_id": bank_id
+    })
+
+    client.post("/branches/", json={
+        "branch_name": "Axis Branch 2",
+        "city":"rangareddy",
+        "ifsc": "AXIS0002",
+        "bank_id": bank_id
+    })
+
+    response = client.get(f"/banks/{bank_id}/branches")
+
 def test_bank_not_found(client):
 
     response = client.get("/banks/99999")
--- tests/test_branches.py
+++ tests/test_branches.py
@@ -3,3 +3,45 @@ def test_get_branches(client):
     response = client.get('/branches')
 
     assert response.status_code == 200
+
+def test_create_branch(client):
+
+    # First create a bank because branch needs bank_id
+    bank_data = {"name": "HDFC"}
+
+    bank_response = client.post("/banks/", json=bank_data)
+
+    bank_id = bank_response.json()["id"]
+
+    branch_data = {
+
+        "branch_name": "HDFC Hyderabad",
+        "city":"Hyderabad",
+        "ifsc": "HDFC0001",
+        "bank_id": bank_id
+    }
+
+    response = client.post("/branches/", json=branch_data)
+
+    assert response.status_code == 201
+    assert response.json()["branch_name"] == "HDFC Hyderabad"
+    assert response.json()["bank_id"] == bank_id
+
+def test_get_branch_by_id(client):
+
+    bank = client.post("/banks/", json={"name": "ICICI"})
+    bank_id = bank.json()["id"]
+
+    branch = client.post("/branches/", json={
+        "branch_name": "ICICI Main",
+        "city":"Chennai",
+        "ifsc": "ICICI0001",
+        "bank_id": bank_id
+    })
+
+    branch_id = branch.json()["id"]
+
+    response = client.get(f"/branches/{branch_id}")
+
+    assert response.status_code == 200
+    assert response.json()["id"] == branch_id
